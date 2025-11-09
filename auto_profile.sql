e182f77057b1524bdde43847fad185acbdff4ee7
Lathitha-20 <lathiesiyekwana@gmail.com>
2025-11-09 17:40:37 +0000

    delete auto profile

--- auto_profile.sql
+++
@@ -1,8 +0,0 @@
--- Adjust name of profile table and foreign key column on profile table
--- before adding this trigger. (Here profile and user_id).
-
-CREATE TRIGGER _new_profile_on_create__user
-AFTER INSERT ON _user
-BEGIN
-    INSERT INTO profile (user_id) VALUES (new.id);
-END;
